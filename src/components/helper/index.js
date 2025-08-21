6f4bf1586a90ba2f4c2ae0091462427f07fdc85b
Vadym Bobenko <bobenko.vadym@gmail.com>
2025-08-21 09:51:54 +0300

    MAUD-9783

--- src/components/helper/index.js
+++ src/components/helper/index.js
@@ -19,60 +19,101 @@ export default class Helper extends PureComponent {
   constructor(props) {
     super(props)
 
-    let { error, focusAnimation } = this.props
+    const { error, focusAnimation, title } = this.props
 
-    let opacity = focusAnimation.interpolate({
+    const opacityFromFocus = focusAnimation.interpolate({
       inputRange: [-1, -0.5, 0],
       outputRange: [1, 0, 1],
       extrapolate: 'clamp',
     })
 
+    this.localOpacity = new Animated.Value(1)
+
+    this.animationValue = 0
+    this._isMounted = false
+
     this.state = {
       errored: !!error,
-      opacity,
+      opacityFromFocus,
+      currentText: !!error ? error : title,
     }
   }
 
   componentDidMount() {
-    let { focusAnimation } = this.props
-
-    this.listener = focusAnimation.addListener(this.onAnimation.bind(this))
+    const { focusAnimation } = this.props
+    this._isMounted = true
+    this.listener = focusAnimation.addListener(this.onAnimation)
   }
 
   componentWillUnmount() {
-    let { focusAnimation } = this.props
-
+    const { focusAnimation } = this.props
+    this._isMounted = false
     focusAnimation.removeListener(this.listener)
+    this._fade && this._fade.stop && this._fade.stop()
   }
 
-  onAnimation({ value }) {
+  onAnimation = ({ value }) => {
     if (this.animationValue > -0.5 && value <= -0.5) {
-      this.setState({ errored: true })
+      this.setState({
+        errored: true,
+        currentText: this.props.error,
+      })
     }
 
     if (this.animationValue < -0.5 && value >= -0.5) {
-      this.setState({ errored: false })
+      this.setState({
+        errored: false,
+        currentText: this.props.title,
+      })
     }
 
     this.animationValue = value
   }
 
-  render() {
-    let { errored, opacity } = this.state
-    let { style, title, error, disabled, baseColor, errorColor } = this.props
+  componentDidUpdate(prevProps) {
+    if (prevProps.error !== this.props.error || prevProps.title !== this.props.title) {
+      const { errored, currentText } = this.state
+      const desiredText = errored ? this.props.error : this.props.title
 
-    let text = errored ? error : title
+      if (desiredText === currentText) return
 
-    if (text == null) {
-      return null
+      const nearSwap = Math.abs((this.animationValue ?? 0) + 0.5) < 0.02
+
+      if (nearSwap) {
+        this.setState({ currentText: desiredText })
+        return
+      }
+
+      this._fade && this._fade.stop()
+      this._fade = Animated.sequence([
+        Animated.timing(this.localOpacity, {
+          toValue: 0,
+          duration: 100,
+          useNativeDriver: true,
+        }),
+        Animated.timing(this.localOpacity, {
+          toValue: 1,
+          duration: 100,
+          useNativeDriver: true,
+        }),
+      ])
+
+      this._fade.start(() => {
+        this.setState({ currentText: desiredText })
+      })
     }
+  }
+
+  render() {
+    let { errored, opacityFromFocus, currentText } = this.state
+    let { style, disabled, baseColor, errorColor } = this.props
 
     let textStyle = {
-      opacity,
+      opacity: opacityFromFocus,
 
-      color: !disabled && errored ? errorColor : style.color || baseColor,
+      color: !disabled && errored ? errorColor : baseColor,
     }
 
-    return <Animated.Text style={[styles.text, style, textStyle]}>{text}</Animated.Text>
+    return <Animated.Text style={[styles.text, style, textStyle]}>{currentText}</Animated.Text>
   }
 }
